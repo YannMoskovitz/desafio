607160524ac375dc46e414e7a237e9ca217f59a2
Yann Moskovitz <yann.moskovitz@hotmail.com>
2021-04-19 16:32:27 -0300

    mudança Services
    criação de algumas funções no ClientServices / ClienteController

--- src/main/java/com/api/dev/apicontabil/controller/ClientController.java
+++ src/main/java/com/api/dev/apicontabil/controller/ClientController.java
@@ -1,9 +1,15 @@
 package com.api.dev.apicontabil.controller;
 
 
+import com.api.dev.apicontabil.model.Client;
 import com.api.dev.apicontabil.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class ClientController {
@@ -11,5 +17,27 @@ public class ClientController {
     @Autowired
     ClientService clientService;
 
+    @GetMapping("/client")
+    Iterable<Client> read() {
+        return clientService.findAll();
+    }
 
+    @GetMapping("/client/{id}")
+    Optional<Client> findById(@PathVariable Integer id) {
+        return clientService.findById(id);
+    }
+
+    @GetMapping("client/search")
+    Iterable<Client> findByQuery(@RequestParam(value = "nome",required = false) String nome,
+                                 @RequestParam(value = "email", required = false) String email) {
+        
+        if (nome != null && email != null)
+            return clientService.findByNomeAndEmail(nome, email);
+        else if (nome != null)
+            return clientService.findByNome(nome);
+        else if (email != null)
+            return  clientService.findByEmail(email);
+        else
+            return clientService.findAll();
+        }
 }
--- src/main/java/com/api/dev/apicontabil/services/ClientService.java
+++ src/main/java/com/api/dev/apicontabil/services/ClientService.java
@@ -1,7 +1,11 @@
 package com.api.dev.apicontabil.services;
 
 import com.api.dev.apicontabil.model.Client;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ClientService extends CrudRepository<Client, Integer> {
+    Iterable<Client> findByNomeAndEmail(String nome, String email);
+    Iterable<Client> findByNome(String nome);
+    Iterable<Client> findByEmail(String email);
 }
