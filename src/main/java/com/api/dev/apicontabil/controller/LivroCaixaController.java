3f1311e86a9cfb716956bd51750407af9f961dd1
Yann Moskovitz <yann.moskovitz@hotmail.com>
2021-04-21 18:46:00 -0300

    crição do livroCaixaControlle/Service e inclusão dos getter/setters no model

---
+++ src/main/java/com/api/dev/apicontabil/controller/LivroCaixaController.java
@@ -0,0 +1,52 @@
+package com.api.dev.apicontabil.controller;
+
+import com.api.dev.apicontabil.model.Client;
+import com.api.dev.apicontabil.model.LivroCaixa;
+import com.api.dev.apicontabil.services.LivroCaixaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+public class LivroCaixaController {
+    @Autowired
+    LivroCaixaService livroCaixaService;
+
+    @GetMapping("/livro")
+    Iterable<LivroCaixa> read(){
+        return livroCaixaService.findAll();
+    }
+
+    @GetMapping("/livro/{id}")
+    Optional<LivroCaixa> findById(@PathVariable Integer id) {
+        return livroCaixaService.findById(id);
+    }
+
+//    @GetMapping("/livro/search")
+//    Iterable<LivroCaixa> findByQuery(@RequestParam(value = "id_cliente", required = false) int id_client){
+//        return livroCaixaService.findById_client(id_client);
+//    }
+
+    @PostMapping("/livro")
+    LivroCaixa create(@RequestBody LivroCaixa livroCaixa) {
+        return livroCaixaService.save(livroCaixa);
+    }
+
+    @PutMapping("/livro")
+    ResponseEntity<LivroCaixa> update(@Valid @RequestBody LivroCaixa livroCaixa) {
+
+        if (livroCaixaService.findById(livroCaixa.getId()).isPresent())
+            return new ResponseEntity(livroCaixaService.save(livroCaixa), HttpStatus.OK);
+        else
+            return new ResponseEntity("O usuario o qual voce tentou atualizar não existe", HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/livro/{id}")
+    void delete(@PathVariable Integer id) {
+        livroCaixaService.deleteById(id);
+    }
+}
--- src/main/java/com/api/dev/apicontabil/model/LivroCaixa.java
+++ src/main/java/com/api/dev/apicontabil/model/LivroCaixa.java
@@ -32,7 +32,51 @@ public class LivroCaixa {
     @Column(name = "valor", length = 13,nullable = false)
     private String valor;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public Client getId_client() {
+        return id_client;
+    }
 
+    public void setId_client(Client id_client) {
+        this.id_client = id_client;
+    }
+
+    public Date getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(Date dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public com.api.dev.apicontabil.model.tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(com.api.dev.apicontabil.model.tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
 }
